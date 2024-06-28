42996dd316cb48221f0c49a111f807d2bff39650
Tom88szc <osobinskitomasz@gmail.com>
2024-06-28 23:32:22 +0200

    initial four

--- main.py
+++ main.py
@@ -16,3 +16,23 @@ def get_all_exception_to_realtime_validation_bnet(self, scenario):
 
     print(scenario_dict)
     return scenario_dict
+
+
+def get_all_data_fields(self):
+    """
+    Return a dictionary with SCENARIO_00X and TESTCASE_00X as keys
+    and DATA as values.
+    """
+
+    file_path = os.path.join(self.work_dir, 'data.yaml')
+    data = read_yaml_file(file_path)
+    scenario_dict = {}
+
+    for sc_index, testcases in data.items():
+        scenario_dict[sc_index] = {}
+        for testcase_key, testcase_value in testcases.items():
+            if str(testcase_key).startswith('TESTCASE_'):
+                scenario_dict[sc_index][testcase_key] = testcase_value['DATA']
+
+    print(scenario_dict)
+    return scenario_dict
